71d4a627b2f5e5c61d8b21d183bd18a67b8b4802
sanmugapriyan <sanmugap545@gmail>
2026-07-01 20:52:34 +0530

    Day 9 Tasks

---
+++ src/Day9/Stringpallindrome.java
@@ -0,0 +1,27 @@
+package Day9;
+import java.util.Arrays;
+public class Stringpallindrome {
+    public static void main(String[]args){
+        String s="SRIRAJ";
+        char[] ch=new char[s.length()];
+        int n=s.length();
+        for(int i=0;i<s.length();i++){
+            ch[i]=s.charAt(n-1-i);
+        }
+        String rev="";
+        for(int i=0;i<ch.length;i++ ){
+
+
+            rev = rev + ch[i];
+        }
+        System.out.println(Arrays.toString(ch));
+if(s.equals(rev)){
+    System.out.println("pallindrome");
+}
+else{
+    System.out.println("Not pallindrome");
+}
+
+
+    }
+}
