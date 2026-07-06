745e1f3f2d03d4b3cdba5095d87d82dd008a8542
sanmugapriyan <sanmugap545@gmail>
2026-07-06 09:49:19 +0530

    Day 1 Home task compeleted using uppercase and lower case

---
+++ src/Day1/Hometask/Uppercase.java
@@ -0,0 +1,17 @@
+package Day1.Hometask;
+
+public class Uppercase {
+    public static void main(String[] args) {
+        char letter = '5';
+        int value = letter;
+        if (value >= 65 && value <= 90) {
+            System.out.println(letter + " is upper");
+        } else if (value >= 97 && value <= 122) {
+            System.out.println(letter + " is lower");
+        } else if (value >= 48 && value <= 57) {
+            System.out.println(letter + " is number");
+        } else {
+            System.out.println(letter + " is sp ch");
+        }
+    }
+}
