0dd41d44fe34a03fabfe8c0e53161e1026d26936
sanmugapriyan <sanmugap545@gmail>
2026-07-06 10:24:28 +0530

    DAY4 FORLOOP PROGRAM

---
+++ src/Day4/For_loop.java
@@ -0,0 +1,9 @@
+package Day4;
+
+public class For_loop {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+    }
+}
