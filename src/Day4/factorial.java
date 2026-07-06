50e0a6912fa4fdd2582c046d0b22151151b43fb1
sanmugapriyan <sanmugap545@gmail>
2026-07-06 10:24:58 +0530

    DAY4 FACTORIAL PROGRAM

---
+++ src/Day4/factorial.java
@@ -0,0 +1,18 @@
+package Day4;
+
+import java.util.Scanner;
+import java.util.Scanner;
+public class factorial {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value:");
+        int n = sc.nextInt();
+        int sum = 1;
+        for (int i = 1; i <= n; i++) {
+            sum = sum * i;
+        }
+        System.out.println(sum);
+    }
+}
+
