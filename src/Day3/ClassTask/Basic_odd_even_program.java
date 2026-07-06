b41369c53dada319a84a6b3f29d164cdf959196b
sanmugapriyan <sanmugap545@gmail>
2026-07-06 10:14:08 +0530

    DAY3 Basic_odd_even_program.java PROGRAM

---
+++ src/Day3/ClassTask/Basic_odd_even_program.java
@@ -0,0 +1,17 @@
+package Day3.ClassTask;
+import java.util.Scanner;
+public class Basic_odd_even_program {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int a = sc.nextInt();
+
+        if (a % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
+        sc.close();
+    }
+}
