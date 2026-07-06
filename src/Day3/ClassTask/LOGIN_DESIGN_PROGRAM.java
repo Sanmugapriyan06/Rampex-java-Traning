e4557652f23c222e6e7233f48561eb80ee516fed
sanmugapriyan <sanmugap545@gmail>
2026-07-06 10:12:37 +0530

    DAY3 LOGIN_DESIGN_PROGRAM

---
+++ src/Day3/ClassTask/LOGIN_DESIGN_PROGRAM.java
@@ -0,0 +1,32 @@
+package Day3.ClassTask;
+import java.util.Scanner;
+public class LOGIN_DESIGN_PROGRAM {
+
+
+        public static void main(String[] args)
+        {
+            Scanner sc =new Scanner(System.in);
+            System.out.println("welcome ");
+            System.out.print("enter your username: ");
+            String username = sc.nextLine();
+            System.out.print("enter password: ");
+            String password = sc.nextLine();
+            if (username.equals("sandy@123"))
+            {
+                if (password.equals("123"))
+                {
+                    System.out.println("login success");
+                }
+                else
+                {
+                    System.out.println("invalid password");
+                }
+            }
+            else
+            {
+                System.out.println("invalid username");
+            }
+
+
+        }
+}
