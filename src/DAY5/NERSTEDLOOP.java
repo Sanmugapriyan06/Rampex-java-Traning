599968d298ab6ef987906f211d6a84efbec18a39
sanmugapriyan <sanmugap545@gmail>
2026-07-06 11:25:44 +0530

    DAY5 NERSTEDLOOP PROGRAM

---
+++ src/DAY5/NERSTEDLOOP.java
@@ -0,0 +1,15 @@
+package DAY5;
+
+public class NERSTEDLOOP {
+    public static void main(String[] args){
+        for(int i=0;i<=5;i++) {//how many times
+
+            for (int j = 1; j <= 5; j++) {//what to print
+                //printing loop 12345
+                System.out.print(i);
+            }
+            System.out.println(" ");
+
+        }
+    }
+}
