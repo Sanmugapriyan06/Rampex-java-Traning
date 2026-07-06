fb5b5a49659c53f6f18200b20ed416fb2d0ea7a2
sanmugapriyan <sanmugap545@gmail>
2026-07-06 10:25:32 +0530

    DAY4 SWITCHCASE PROGRAM

---
+++ src/Day4/Switch_case.java
@@ -0,0 +1,44 @@
+package Day4;
+import java.util.Scanner;
+public class Switch_case {
+
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the day number :");
+            int a= sc.nextInt();
+            switch(a){
+                case 1 :{
+                    System.out.println("sunday");
+                    break;
+                }
+                case 2:{
+                    System.out.println("monday");
+                    break;
+                }
+                case 3:{
+                    System.out.println("tuesday");
+                    break;
+                }
+                case 4:{
+                    System.out.println("wednesday");
+                    break;
+                }
+                case 5:{
+                    System.out.println("thursday");
+                    break;
+                }
+                case 6:{
+                    System.out.println("friday");
+                    break;
+                }
+                case 7:{
+                    System.out.println("saturday");
+                    break;
+                }
+                default:{
+                    System.out.println("invalid day");
+                }
+            }
+            sc.close();
+        }
+}
