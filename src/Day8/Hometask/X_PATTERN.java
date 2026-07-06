dc50c5070aac92f3b09c86bd130bfbadcd152670
sanmugapriyan <sanmugap545@gmail>
2026-07-06 12:07:32 +0530

    DAY8 x pattern program

---
+++ src/Day8/Hometask/X_PATTERN.java
@@ -0,0 +1,20 @@
+package Day8.Hometask;
+import java.util.Scanner;
+public class X_PATTERN {
+
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter the number: ");
+            int n=sc.nextInt();
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(i==j || i+j==n-1){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                } System.out.println();
+            }
+        }
+}
