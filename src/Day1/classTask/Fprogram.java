6539202dc35acc4c941cedd9a4b6cace1f52b79e
sanmugapriyan <sanmugap545@gmail>
2026-07-06 09:48:00 +0530

    Day 1 class task compeleted using print function to display the name

---
+++ src/Day1/classTask/Fprogram.java
@@ -0,0 +1,8 @@
+package Day1.classTask;
+
+public class Fprogram {
+    public static void main(String[] args)
+    {
+        System.out.println(" This is my first program ' Sanmugapriyan ' ");
+    }
+}
