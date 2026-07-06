f38c3a6dfdb7579713c864cbf877f6fa52fdbd7e
sanmugapriyan <sanmugap545@gmail>
2026-07-06 14:41:27 +0530

    DAY13 EXAMPLE CODE

---
+++ src/Day13/Examplecode.java
@@ -0,0 +1,17 @@
+package Day13;
+import java.util.Scanner;
+public class Examplecode {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the values:");
+        try {
+            int a = sc.nextInt();
+            System.out.println(a*a);
+
+        } catch (Exception e) {
+
+        }
+    }
+}
+
+
