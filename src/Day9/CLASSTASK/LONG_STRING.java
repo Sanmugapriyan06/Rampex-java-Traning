a1cfd242e20908e57950f97063c90382f88cc940
sanmugapriyan <sanmugap545@gmail>
2026-07-06 12:18:47 +0530

    DAY9 long string prigram

---
+++ src/Day9/CLASSTASK/LONG_STRING.java
@@ -0,0 +1,23 @@
+package Day9.CLASSTASK;
+
+public class LONG_STRING {
+        public static void main(String[] args) {
+            int maxlength = 0;
+            String n = "";
+            String[] name = new String[]{"ramesh", "kishore", "ramya", "priya"};
+
+            for (int i = 0; i < name.length; ++i) {
+                if (name[i].length() > maxlength) {
+                    maxlength = name[i].length();
+                }
+
+                if (name[i].length() == maxlength) {
+                    n = name[i];
+                }
+            }
+
+            System.out.println(n);
+            System.out.println(maxlength);
+        }
+    }
+
