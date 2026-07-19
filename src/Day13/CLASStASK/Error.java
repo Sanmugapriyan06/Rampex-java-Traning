7a1ad11691d188ee1e928cdad274e1237fe6c6a2
sanmugapriyan <sanmugap545@gmail>
2026-07-19 19:51:10 +0530

    DAY13 Error.java program code

---
+++ src/Day13/CLASStASK/Error.java
@@ -0,0 +1,13 @@
+package Day13.CLASStASK;
+import java.util.Scanner;
+public class Error {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int c=5;
+        int x=a/(b+c);
+        System.out.println(x);
+        int y=a/(b-c);
+        System.out.println(y);
+    }
+}
